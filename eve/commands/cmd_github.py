480c13b51937e5bcad64ef843e74ffd24f51d8f7
Andrey Shtamburg <andrewshtamburg@gmail.com>
2021-04-27 18:52:24 +0200

    Created main.yml
    - Created basic flow for building and testing the app
    
    fixing typo
    
    Added new utility to get current price for given crypto
    
    Readme update with new crypto command
    
    PR fixes
    
    small typos
    
    Fetch events from google calendar
    
    Docs update
    
    RP fixes
    
    adding crypto gecko fallback
    
    Update README.md and remove pre-commit hook
    
    Update requirements.txt
    
    Added module quote to the requirements.txt
    
    Update config,py
    
    Added default value for quote search to config.py
    
    Add file cmd_quote.py
    
    Add file cmd_quote.py
    
    Add file svc_quote.py
    
    Add file svc_quote.py.
    
    Update README.md
    
    Added a description for the quote command.
    
    Update cmd_quote.py
    
    Removed command `random`.
    
    Update README.md
    
    Changed README.md because of removing the `random` command.
    
    Basic end to end tests for crypto module
    
    Reformat the file with black and isort

--- eve/commands/cmd_github.py
+++ eve/commands/cmd_github.py
@@ -18,7 +18,13 @@ def cli(ctx):
 @cli.command()
 @click.argument("name", type=str)
 @click.option("-p", "--private", type=bool, help="Is the repo private?", default=False)
-@click.option("-i", "--gitignore", type=str, help="The .gitignore file to add to the repo", default="")
+@click.option(
+    "-i",
+    "--gitignore",
+    type=str,
+    help="The .gitignore file to add to the repo",
+    default="",
+)
 @click.pass_context
 def create(ctx, name, private, gitignore):
     """Create a github repo"""
@@ -34,7 +40,9 @@ def create(ctx, name, private, gitignore):
 def delete(ctx, name):
     """Delete a github repo"""
     repo = ctx.obj.github.get_repo(name)
-    if click.confirm(f"Are you sure you want to delete {repo.html_url}?", default=False):
+    if click.confirm(
+        f"Are you sure you want to delete {repo.html_url}?", default=False
+    ):
         ctx.obj.github.delete_repo(repo.name)
         click.echo(f"Deleted: {repo.name}")
     else:
